cc3a0a0be5149031e4da08a6bcf2f17d179da6e7
GonzaRomera <92063887+GonzaRomera@users.noreply.github.com>
2021-10-21 21:00:25 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+/*let intentos: number = 1;
+let clave: string;
+
+while (intentos <= 3 && clave !== "eureka") {
+  clave = prompt("Introduzca la clave");
+  intentos++;
+}
+if (clave === "eureka") {
+  console.log("Clave correcta");
+} else {
+  console.log("Ya no tiene más intentos");
+}
+*/
+/*let m: number;
+let a: string = "";
+for (let m: number = 0; m <= 100; m++)
+  if (m % 2 === 0) {
+    console.log(m);
+  }else{ (m % 3 === 0){
+    console.log(m);
+  }*/
+/*let numIngresado = Number(prompt("Ingrese un número"));
+  while (numIngresado <= 0) {
+  let numIngresado = Number(prompt("Ingrese otro número (positivo)"));
+  }
+  if (numIngresado % 2 == 0) {
+  console.log( "El número es par" );
+  } else {
+  console.log( "El número es impar" );
+  }*/
